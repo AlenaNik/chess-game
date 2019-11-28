9ef506fd4240a3efcea1ce6ca10bff621885dfad
AlenaNik <alena.nik.digital@gmail.com>
2019-11-28 22:06:23 +0100

    3 new func, new color, we can see which figures are moving

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 let map = Array ();
 let inf = Array ();
+
+let move_color = "black";
+
 function init_map() {
     map = [
         // y0    y1    y2   y3   y4    y5    y6   y7
@@ -23,13 +26,38 @@ function init_inf() {
             [" ", " ", " ", " ", " ", " ", " ", " "],
             [" ", " ", " ", " ", " ", " ", " ", " "],
             [" ", " ", " ", " ", " ", " ", " ", " "],
-            [" ", "1", "2", "2", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " ", " ", " ", " "],
             [" ", " ", " ", " ", " ", " ", " ", " "],
             [" ", " ", " ", " ", " ", " ", " ", " "],
             [" ", " ", " ", " ", " ", " ", " ", " "],
         ];
 }
+// func that checks all matrix and sees if we can move
+// from the cell
+function mark_moves_from() {
+    init_inf();
+    for (let x = 0; x <= 7; x++)
+        for (let y = 0; y <= 7; y++)
+            if (can_move_from (x, y))
+                // you you can move from that sell, put 1
+                inf [x] [y] = 1;
+}
+function can_move_from(x, y) {
+    // if it's the same color, we can move
+   return (get_color (x, y) === move_color)
+}
+// check the color of the figure: black, white or empty
+function get_color(x, y) {
+    let figure = map [x] [y];
+    if (figure === '')
+        return "";
+    // since we know that the upper case is white, we check if
+    // figure is equal to itself, so it's white
+    return (figure.toUpperCase() === figure) ? "white" : "black";
+}
 function show_map() {
+    // creating 2D arrays https://p5js.org/examples/arrays-array-2d.html
+
     layout = "<table>";
     for (let y = 7; y >= 0; y--) {
         layout += "<tr>";
@@ -48,5 +76,5 @@ function show_map() {
     }
 }
 init_map();
-init_inf();
+mark_moves_from();
 show_map();
