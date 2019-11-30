20b58998b74a57f31458e69e60f85e3f207d9e57
AlenaNik <alena.nik.digital@gmail.com>
2019-11-30 12:32:28 +0100

    where we can move to?

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 let map = Array ();
 let inf = Array ();
 
-let move_color = "black";
+let move_color = "white";
+let move_from_x;
+let move_from_y;
 
 function init_map() {
     map = [
@@ -32,20 +34,38 @@ function init_inf() {
             [" ", " ", " ", " ", " ", " ", " ", " "],
         ];
 }
-// func that checks all matrix and sees if we can move
+// func that checks all matrix and sees if we can move FROM (1)
 // from the cell
 function mark_moves_from() {
     init_inf();
     for (let x = 0; x <= 7; x++)
         for (let y = 0; y <= 7; y++)
             if (can_move_from (x, y))
-                // you you can move from that sell, put 1
+                // you you can move FROM that sell, put 1 (TO is going to be two)
                 inf [x] [y] = 1;
 }
+
+// func that: 1) clears inif_inf, than checkes if we can move TO (2)
+function mark_moves_to() {
+    init_inf();
+    for (let x = 0; x <= 7; x++)
+        for (let y = 0; y <= 7; y++)
+            if (can_move_to (x, y))
+            // you you can move FROM that sell, put 1 (TO is going to be two)
+                inf [x] [y] = 2;
+}
+
 function can_move_from(x, y) {
     // if it's the same color, we can move
    return (get_color (x, y) === move_color)
 }
+function can_move_to(x, y) {
+    // white figure can only fo on empty cell of on black figure
+    if (map [x] [y] == " ")  // is it empty?
+        return true;
+    return get_color (x, y) !== move_color; // is it different color?
+}
+
 // check the color of the figure: black, white or empty
 function get_color(x, y) {
     let figure = map [x] [y];
@@ -55,6 +75,28 @@ function get_color(x, y) {
     // figure is equal to itself, so it's white
     return (figure.toUpperCase() === figure) ? "white" : "black";
 }
+
+function click_box (x, y) {
+   if (inf [x] [y] == "1")
+       click_box_from (x, y);
+   if (inf [x] [y] == "2")
+       click_box_to (x, y);
+}
+
+// func that does the following: click on cell,save coordinates,mark where to go and display changes
+function click_box_from (x, y) {
+    // saving where we are coming from
+    move_from_x = x;
+    move_from_y = y;
+    // where we want to move
+    mark_moves_to ();
+    //  display on map
+    show_map();
+}
+function click_box_to (x, y) {
+
+}
+
 function show_map() {
     // creating 2D arrays https://p5js.org/examples/arrays-array-2d.html
 
@@ -67,7 +109,9 @@ function show_map() {
                 color = (x + y) % 2 ? "white" : "grey";
             else
                 color = inf[x] [y] === "1" ? "green" : "#62F9FE";
-            layout += "<td class='board' style='background-color: " + color + "'>";
+            layout += "<td class='board' style='background-color: " + color +
+               //Coordinated of x and y at the moment of click
+                "' onclick='click_box(" + x + ", " + y + ");'>";
             layout += map [x] [y];
                layout += "</td>";
         }
